5bc11357dc5c4fb8e49aa56c379a8730735553de
caizhenghe <caizhenghe@bilibili.com>
2020-12-20 21:23:01 +0800

    Fix compile issue to log avcodec config

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,17 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include "libavcodec/avcodec.h"
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_studio_jmpeg_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+//    return env->NewStringUTF(hello.c_str());
+    char info[10000] = {0};
+    sprintf(info, "%s\n", avcodec_configuration());
+    return env->NewStringUTF(info);
+}
 }
--- app/src/main/java/com/studio/jmpeg/MainActivity.kt
+++ app/src/main/java/com/studio/jmpeg/MainActivity.kt
@@ -22,6 +22,14 @@ class MainActivity : AppCompatActivity() {
     companion object {
         // Used to load the 'native-lib' library on application startup.
         init {
+            System.loadLibrary("avcodec-57");
+            System.loadLibrary("avdevice-57");
+            System.loadLibrary("avfilter-6");
+            System.loadLibrary("avformat-57");
+            System.loadLibrary("avutil-55");
+            System.loadLibrary("postproc-54");
+            System.loadLibrary("swresample-2");
+            System.loadLibrary("swscale-4");
             System.loadLibrary("jmpeg")
         }
     }
